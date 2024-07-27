b2f6b940f013f65b9754d444709db8048cc8828c
Elias Eduardo Liranza Pérez <146482192+eliaselp@users.noreply.github.com>
2024-07-27 14:02:30 -0700

    Update formularios.py

--- App/formularios.py
+++ App/formularios.py
@@ -6,11 +6,13 @@ class LoginForm(forms.Form):
         widget=forms.TextInput(attrs={
             'class': 'form-control',
             'id': 'login-username',
+            'placeholder':"Enter your username"
         })
     )
     password = forms.CharField(
         widget=forms.PasswordInput(attrs={
             'class': 'form-control',
             'id': 'login-password',
+            'placeholder':"Enter your password"
         })
     )

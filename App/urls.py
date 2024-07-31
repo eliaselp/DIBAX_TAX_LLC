d259a0db6101a3d68dad3a619d8abbf1901d7f5a
Elias Eduardo Liranza Pérez <146482192+eliaselp@users.noreply.github.com>
2024-07-30 18:30:53 -0700

    Delete App directory

--- App/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path,include
-from . import views
-urlpatterns = [
-    path('',views.Login.as_view()),
-    path('logout/',views.Logout.as_view()),
-    path('home/', views.Index.as_view()),
-]
